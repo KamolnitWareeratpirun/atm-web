22a0bea113e7938d3268acea98b2c368fa8466a1
unknown <paikwp@gmail.com>
2020-10-18 13:51:32 +0700

    61050811

---
+++ src/main/java/th/ac/kmitl/atm/service/CustomerService.java
@@ -0,0 +1,32 @@
+package th.ac.kmitl.atm.service;
+
+import org.springframework.stereotype.Service;
+import th.ac.kmitl.atm.model.Customer;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomerService
+{
+    private List<Customer> customerList;
+
+    @PostConstruct
+    public void postConstruct()
+    {
+        this.customerList = new ArrayList<>();
+    }
+
+    public void createCustomer(Customer customer)
+    {
+        // ...has pin for customer...
+        customerList.add(customer);
+    }
+
+    public List<Customer> getCustomers()
+    {
+        return new ArrayList<>(this.customerList);
+    }
+
+}
